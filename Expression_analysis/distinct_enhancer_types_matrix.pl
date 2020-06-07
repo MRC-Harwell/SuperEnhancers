4e9c2a34de4ead239056345b652cb6632535153f
sid-sethi <28432703+sid-sethi@users.noreply.github.com>
2020-06-07 18:43:29 +0100

    Update distinct_enhancer_types_matrix.pl

--- Expression_analysis/distinct_enhancer_types_matrix.pl
+++ Expression_analysis/distinct_enhancer_types_matrix.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl
 # making of the matrix of super enhancers and typical enh for cell type specificity (distinct enhancer types)
-# this script was also used to make enhacer matrix for Random forest
 #Author: Sid
 
 use Getopt::Long;
@@ -35,11 +34,8 @@ if( !$path || $path =~ /\./ ) { $cwd = getcwd ; $path = "$cwd" ; }
 # Gene list - all super enhancers genes/ typical enh genes
 my $gene_file = "/NGS/users/Sid/ENHANCERS/SuperEnh_27ac/Great_targets/Strict_targets/CellTypeSpecific_analysis/all.te.uniq.genes.txt" ;
 
-#my $gene_file = " /NGS/users/Sid/ChromHmm/Posterior/State_6/POSTERIOR/Tau_enhancers/TauResults/Highly_specific/SuperEnh/AllGenes_super/new_pc_genes_list.txt" ;
-
 # super enhancers /typical enhancers
 my $dir= "/NGS/users/Sid/ENHANCERS/SuperEnh_27ac/Great_targets/Strict_targets" ;
-#my $dir = "/NGS/users/Sid/ENHANCERS/SuperEnh_27ac/Great_targets/Strict_targets/Ppi_scores" ;
 
 my @tissues = (
 	"BAT",
@@ -77,20 +73,12 @@ foreach my $gene(<in>){
 		#print "$tis ...\n" ;
 		$count++ ;
 		my $result ;
-		#my $lines = `grep -w $gene $dir/$tis.ppiScore.txt | wc -l` ; chomp($lines) ;
     my $lines = `grep -w $gene $dir/$tis.te.uniq.genes.txt | wc -l` ; chomp($lines) ;
     if($lines == 0) {
 			$result = 0 ;
 		}
 		else {
 			$result = 1 ;
-      # my $grep = `grep -w $gene $dir/$tis.ppiScore.txt` ;
-      # my @grep = split("\n",$grep) ;
-	    # foreach my $l(@grep){
-      #   $l =~ s/\n|\r|\s+$//g ;
-      #   my($g,$sc) = split("\t",$l) ;
-      #   $result = $sc ;
-      #}
 		}
 		if($count == 22){
 			print "$result" ;
